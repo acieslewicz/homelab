bbc23f62138e00ab3e508646bc32bb459dbc4b90
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-03-16 22:00:31 -0600

    feat: Add provisioning of proxy container

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -54,6 +54,46 @@ resource "proxmox_virtual_environment_container" "dns1" {
   features {
     nesting = true
   }
-
 }
 
+resource "proxmox_virtual_environment_container" "proxy1" {
+
+  node_name    = "HOME-HYP-001"
+  unprivileged = true
+
+  tags = ["proxy"]
+
+  initialization {
+    hostname = "HOME-PROXY-001"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.100.20/24"
+        gateway = "192.168.100.1"
+      }
+    }
+
+    user_account {
+      keys = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  network_interface {
+    name = "veth0"
+  }
+
+
+  operating_system {
+    template_file_id = proxmox_virtual_environment_download_file.latest_debian_12_bookworm_lxc_img.id
+    type             = "debian"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 20
+  }
+
+  features {
+    nesting = true
+  }
+}
