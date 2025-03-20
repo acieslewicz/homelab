9012bf99467651d1f5b930ab749301fd3719246c
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-03-20 15:12:24 -0600

    feat: Add cloudflare tunnel config

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -97,3 +97,46 @@ resource "proxmox_virtual_environment_container" "proxy1" {
     nesting = true
   }
 }
+
+resource "proxmox_virtual_environment_container" "tunner1" {
+
+  node_name    = "HOME-HYP-001"
+  unprivileged = true
+
+  tags = ["tunnel"]
+
+  initialization {
+    hostname = "EXT-TUNNEL-001"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.110.10/24"
+        gateway = "192.168.110.1"
+      }
+    }
+
+    user_account {
+      keys = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  network_interface {
+    name    = "veth0"
+    vlan_id = 110
+  }
+
+
+  operating_system {
+    template_file_id = proxmox_virtual_environment_download_file.latest_debian_12_bookworm_lxc_img.id
+    type             = "debian"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 20
+  }
+
+  features {
+    nesting = true
+  }
+}
