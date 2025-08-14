ccb6aa55d2ec23d9517abfb501656f93f54bb5b7
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-08-14 20:10:09 +0300

    Setup gateway infrastructure

---
+++ opentofu/proxmox/gateway.tf
@@ -0,0 +1,26 @@
+resource "proxmox_virtual_environment_vm" "gateway" {
+  name      = "gateway"
+  node_name = "proxmox-01"
+  tags      = ["tofu", "ansible", "docker", "periphery"]
+
+  clone {
+    vm_id = proxmox_virtual_environment_vm.debian_template.id
+  }
+
+  agent {
+    enabled = true
+  }
+
+  initialization {
+    dns {
+      domain  = "home.tinymagellanic.cloud"
+      servers = ["9.9.9.9", "149.112.112.112"]
+    }
+    ip_config {
+      ipv4 {
+        address = "192.168.100.50/24"
+        gateway = "192.168.100.1"
+      }
+    }
+  }
+}
